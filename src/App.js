cc71c516ddde638d86655a5f730dfff47a73aad6
rboddy <avidryanb@gmail.com>
2019-03-30 20:14:41 -0400

    adding the store and provider to app.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import "./App.css";
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./components/layout/Dashboard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <AppNavbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Dashboard} />
-            </Switch>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <AppNavbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
