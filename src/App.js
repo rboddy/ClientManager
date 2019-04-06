f7fbfba346cd51ce77c380b861e10233ae8287d5
rboddy <avidryanb@gmail.com>
2019-04-06 09:31:47 -0400

    Section 10 Lecture 63: adding in the edit client functionality from the button in the details page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./components/layout/Dashboard";
 import AddClient from "./components/clients/AddClient";
+import EditClient from "./components/clients/EditClient";
 import ClientDetails from "./components/clients/ClientDetails";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -19,6 +20,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
               </Switch>
             </div>
---
+++ src/components/clients/EditClient.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { reactReduxFirebase, firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
+
+class EditClient extends Component {
+  constructor(props) {
+    super(props);
+    // Create Refs
+    this.firstNameInput = React.createRef();
+    this.lastNameInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.phoneInput = React.createRef();
+    this.balanceInput = React.createRef();
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { client, firestore, history } = this.props;
+
+    // Updated Client
+    const updClient = {
+      firstName: this.firstNameInput.current.value,
+      lastName: this.lastNameInput.current.value,
+      email: this.emailInput.current.value,
+      phone: this.phoneInput.current.value,
+      balance:
+        this.balanceInput.current.value == ""
+          ? 0
+          : this.balanceInput.current.value
+    };
+
+    //Update in firestore
+    firestore
+      .update({ collection: "clients", doc: client.id }, updClient)
+      .then(history.push("/"));
+  };
+  render() {
+    const { client } = this.props;
+    if (client) {
+      return (
+        <div>
+          <div class="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn-link">
+                <i class="fas fa-arrow-circle-left" /> Back to Dashboard
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">Edit Client</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="firstName"
+                    minLength="2"
+                    required
+                    ref={this.firstNameInput}
+                    defaultValue={client.firstName}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="lastName"
+                    minLength="2"
+                    required
+                    ref={this.lastNameInput}
+                    defaultValue={client.lastName}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    name="email"
+                    ref={this.emailInput}
+                    defaultValue={client.email}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="phone"
+                    minLength="10"
+                    required
+                    ref={this.phoneInput}
+                    defaultValue={client.phone}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="balance">Balance</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="balance"
+                    ref={this.balanceInput}
+                    defaultValue={client.balance}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  defaultValue="Submit"
+                  className="btn btn-primary btn-block"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <Spinner />;
+    }
+  }
+}
+
+EditClient.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default compose(
+  firestoreConnect(props => [
+    { collection: "clients", storeAs: "client", doc: props.match.params.id }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0]
+  }))
+)(EditClient);
