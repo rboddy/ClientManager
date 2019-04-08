249441b2e44dd15c82e2684f731e9d3689584e5f
rboddy <avidryanb@gmail.com>
2019-04-08 06:46:43 -0400

    Section 10 Lecture 64: Adding login functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./components/layout/Dashboard";
 import AddClient from "./components/clients/AddClient";
 import EditClient from "./components/clients/EditClient";
 import ClientDetails from "./components/clients/ClientDetails";
+import Login from "./components/auth/Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/edit/:id" component={EditClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </div>
