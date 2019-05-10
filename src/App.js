c675ada86a395ce433a37009416873ff56bbda26
rboddy <avidryanb@gmail.com>
2019-05-10 07:53:16 -0400

    Final app commit: I locked down the registration route and deployed to firebase. We are live. Exciting!

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddClient from "./components/clients/AddClient";
 import EditClient from "./components/clients/EditClient";
 import ClientDetails from "./components/clients/ClientDetails";
 import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Settings from "./components/settings/Settings";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { UserIsAuthenticated, UserIsNotAuthenticated } from "./helpers/auth";
@@ -50,6 +51,11 @@ class App extends Component {
                   exact
                   path="/login"
                   component={UserIsNotAuthenticated(Login)}
+                />
+                <Route
+                  exact
+                  path="/register"
+                  component={UserIsNotAuthenticated(Register)}
                 />
               </Switch>
             </div>
